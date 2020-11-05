d02e94b33ac496d893d256d3181ae7be20a2778b
DESKTOP-MF8U07A\Jelena <jelena.popadic.1999@gmail.com>
2020-11-05 23:32:50 +0100

    commit 3: kreiranje klase

---
+++ Podaci o kolokvijumu/Podaci o kolokvijumu/ExerciseResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Podaci_o_kolokvijumu
+{
+    class ExerciseResult
+    {
+        public ExerciseResult(int id, string studentName, string studentIndex, int studentPoints)
+        {
+            this.id = id;
+            StudentName = studentName;
+            StudentIndex = studentIndex;
+            StudentPoints = studentPoints;
+        }
+
+        public int id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int StudentPoints { get; set; }
+    }
+}
